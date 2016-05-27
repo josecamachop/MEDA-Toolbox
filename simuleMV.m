8bfebe1be8f48e3a47ed0ac184219c013c38dd01
José Camacho <josecamacho@ugr.es>
2016-05-27 21:28:52 +0200

    New simuleMV version

--- simuleMV.m
+++ simuleMV.m
@@ -1,11 +1,11 @@
-function X = simuleMV(obs,vars,corr,lcorr)
+function X = simuleMV(obs,vars,cor,lcorr)
 
 % Simulation of MV data with ADICOV (Approximation of a DIstribution for a 
 % given COVariance, Chemometrics and Intelligent Laboratory Systems 105(2), 
 % 2011, pp. 171-180.
 %
 % X = simuleMV(obs,vars) % minimum call
-% X = simuleMV(obs,vars,corr,lcorr)% complete call
+% X = simuleMV(obs,vars,cor,lcorr)% complete call
 %
 %
 % INPUTS:
@@ -14,11 +14,10 @@ function X = simuleMV(obs,vars,corr,lcorr)
 %
 % vars: [1x1] number of variables (columns) in the output.
 %
-% corr: [vars x vars] pre-specified correlation. NANs in this input are 
+% cor: [vars x vars] pre-specified correlation. NANs in this input are 
 %   replaced by random values. All random ( nan(vars) ) used by default.
 %
-% lcorr: [1x1] level of correlation among variables, in [0,100] (10 by 
-%   default). The correlation used is equal to: corr.^(2*lcorr+1).
+% lcorr: [1x1] level of correlation among variables in [0,10] (5 by default) 
 %
 %
 % OUTPUTS:
@@ -36,14 +35,14 @@ function X = simuleMV(obs,vars,corr,lcorr)
 % EXAMPLE OF USE: Matrix 100x10 with the first five variables with 
 % correlation 1:
 %
-% corr = nan(10);
-% corr(1:5,1:5) = 1;
-% X = simuleMV(100,10,corr);
+% cor = nan(10);
+% cor(1:5,1:5) = 1;
+% X = simuleMV(100,10,cor);
 % meda_pca(X); % visualization (auto-scaled data)
 %
 %
 % coded by: Jose Camacho Paez (josecamacho@ugr.es)
-% last modification: 25/May/16.
+% last modification: 27/May/16.
 %
 % Copyright (C) 2014  University of Granada, Granada
 % Copyright (C) 2014  Jose Camacho Paez
@@ -66,14 +65,14 @@ function X = simuleMV(obs,vars,corr,lcorr)
 % Set default values
 routine=dbstack;
 assert (nargin >= 2, 'Error in the number of arguments. Type ''help %s'' for more info.', routine(1).name);
-if nargin < 3 || isempty(corr), corr = nan(vars); end;
-ind = find(~isnan(corr));
-if nargin < 4 || isempty(lcorr), lcorr = 10; end;
+if nargin < 3 || isempty(cor), cor = nan(vars); end;
+ind = find(~isnan(cor));
+if nargin < 4 || isempty(lcorr), lcorr = 5; end;
 
 % Validate dimensions of input data
 assert (isequal(size(obs), [1 1]), 'Dimension Error: 1st argument must be 1-by-1. Type ''help %s'' for more info.', routine(1).name);
 assert (isequal(size(vars), [1 1]), 'Dimension Error: 2nd argument must be 1-by-1. Type ''help %s'' for more info.', routine(1).name);
-assert (isequal(size(corr), [vars vars]), 'Dimension Error: 3rd argument must be vars-by-vars. Type ''help %s'' for more info.', routine(1).name);
+assert (isequal(size(cor), [vars vars]), 'Dimension Error: 3rd argument must be vars-by-vars. Type ''help %s'' for more info.', routine(1).name);
 assert (isequal(size(lcorr), [1 1]), 'Dimension Error: 4th argument must be 1-by-1. Type ''help %s'' for more info.', routine(1).name);
 
 % Validate values of input data
@@ -82,17 +81,21 @@ assert (isequal(fix(obs), obs), 'Value Error: 1st argument must be an integer. T
 assert (vars>0, 'Value Error: 2nd argument must be above 0. Type ''help %s'' for more info.', routine(1).name);
 assert (isequal(fix(vars), vars), 'Value Error: 2nd argument must be an integer. Type ''help %s'' for more info.', routine(1).name);
 if ~isempty(ind), 
-    assert (isempty(find(corr(ind)<-1)), 'Value Error: 3rd argument elements must be equal or above -1. Type ''help %s'' for more info.', routine(1).name);
-    assert (isempty(find(corr(ind)>1)), 'Value Error: 3rd argument elements must be equal or below 1. Type ''help %s'' for more info.', routine(1).name);
+    assert (isempty(find(cor(ind)<-1)), 'Value Error: 3rd argument elements must be equal or above -1. Type ''help %s'' for more info.', routine(1).name);
+    assert (isempty(find(cor(ind)>1)), 'Value Error: 3rd argument elements must be equal or below 1. Type ''help %s'' for more info.', routine(1).name);
 end
-assert (lcorr>=0, 'Value Error: 4th argument must be equal or above 0. Type ''help %s'' for more info.', routine(1).name);
-assert (lcorr<=100, 'Value Error: 4th argument must be equal or below 100. Type ''help %s'' for more info.', routine(1).name);
+assert (lcorr >= 0, 'Value Error: 4th argument must be above or equal to 0. Type ''help %s'' for more info.', routine(1).name);
+assert (lcorr<=10, 'Value Error: 4th argument must be equal to or below 10. Type ''help %s'' for more info.', routine(1).name);
 
 
 %% Main code
 
-COV = randn(vars).^(2*lcorr+1);
+COV1 = 2*rand(vars)-1;
+X = real(ADICOV(COV1,randn(12-lcorr,vars),vars));
+COV = corr(X);
+COV = lcorr*(log10(vars)*2-1)*10*COV + COV1;
 COV = COV./max(max(abs(COV)));
-COV(ind) = corr(ind);
-COV = COV - diag(diag(COV)) + eye(vars);
+COV(ind) = cor(ind);
 X = real(ADICOV(COV,randn(obs,vars),vars));
+
+
