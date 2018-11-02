c1eb39da76673eebdee828d6601d2e329c9b523e
José Camacho <josecamacho@ugr.es>
2018-11-02 09:07:59 +0100

    Adding work by Edo and Raffa for the selection of PCs, and reviewing documentation

---
+++ PAtest.m
@@ -0,0 +1,212 @@
+function [NoComp, PERCENTILES, Eigs2test]  = PAtest(X,alpha,method,type,corrtype,dispopt)
+
+% Code for performing Horn's Parallel Analysis. Parellel analysis is a well
+% establish method to determine the number of Principal components in PCA.
+% The performance of this method and its theoretical underpinning have been
+% reviwed in  Saccenti, E; Timmerman, M. Considering Horn’s Parallel Analysis 
+% from a Random Matrix Theory Point of View Psychometrika 82 (2017)1 
+% http://dx.doi.org/10.1007/s11336-016-9515-z. Plese cite this paper if
+% using this code
+%
+% [NoComp, PERCENTILES, Eigs2test]  = PAtest(X,alpha,method,type,corrtype,dispopt)
+%       complete call
+%
+% 
+% INPUTS
+% 
+% X:        n x p data matrix
+% alpha:    confidence threshold in ]0 1[
+% method:   shuffle:        PA with reshuffling of input data X
+%           random:         PA with random data generated under N(0,I)
+% type:     covariance:     PA using covariance matrix
+%           correlation:    PA using correlation marix
+% corrtype  Type of correlation to be used such as spearman or pearson. See
+%           Matlab help for correlation type available (type help corr)
+% dispopt:  0:              NO graphical display of results
+%           >0:              Graphical display of results
+% 
+%
+% OUTPUTS
+%
+% NoComp:       Number of signifciant component at the alpha elvel scalar)
+% PERCENTILES:  (1-alpha) percentiles of the distributions of the 1st,
+%               2nd,... eigenvalues of the covariance/correlation matrix
+%               (vector)
+% Eigs2test:    eigenvalues of the covariance/correlation matrix (vector)
+% 
+% EXAMPLE OF USE
+% 
+% SIGMA = eye(10,10);
+% SIGMA(1,1) = 5; SIGMA(2,2) = 3;
+% MU = zeros(1,10);
+% X = mvnrnd(MU,SIGMA,100)
+%
+%
+% [NoComp, PERCENTILES, Eigs2test]  = PAtest(X,0.05,'covariance','random','',1)
+% [NoComp, PERCENTILES, Eigs2test]  = PAtest(X,0.05,'correlation','random','pearson',1)
+
+% coded by: Edoardo Saccenti (esaccenti@gmail.com)
+% last modification: 22/July/18
+%
+% Copyright (C) 2018  Wageningen Univeristy & Research
+% Copyright (C) 2018  Edoardo Saccenti
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+% Main Code
+
+%% Arguments checking
+
+routine=dbstack;
+nargin
+%assert ((nargin > 6), 'Error in the number of arguments. Type ''help %s'' for more info.', routine(1).name);
+assert ((nargin == 6) || (nargin == 5) , 'Error in the number of arguments. Type ''help %s'' for more info.', routine(1).name);
+
+%% Checking input
+if nargin < 6
+    dispopt = 0;
+end
+
+if alpha > 1 || alpha < 0
+    error('Alpha must be in ]0,1[')
+end
+
+%% Set some variable
+[nobs, nvar] = size(X);
+Nrep = 2000;
+RAND_EIG = NaN*ones(Nrep, nvar);
+
+%%
+disp('Generating random data and percentiles')
+
+switch lower(method)
+    
+    case 'shuffle' %PA with reshuffling of input data X
+        
+        for qq = 1 : Nrep
+            
+            disp(qq)
+            
+            %Generate "radom" data by shufflig the entries of each column
+            %independently. XN columns have same variance of the columns in
+            %X.
+            XN = NaN*ones(size(X));
+            for d = 1:size(X,2)
+                XN(:, d) = X(randperm(size(X,1)), d);
+            end
+            
+            
+            %Compute covariance/correlation matrix or the "shuffled" data
+            switch lower(type)
+                case 'covariance'
+                    CN = XN'*XN;
+                case 'correlation'
+                    CN = corr(XN, 'type', corrtype);
+                otherwise
+                    error('Type not supported')
+            end
+            
+            %Calculate eigenvalues
+            RAND_EIG(qq,:) = sort(eig(CN),'descend')';
+            
+        end
+        
+    case 'random' %PA with random data generated under N(0,I)
+        
+        
+        for qq = 1 : Nrep
+            
+            disp(qq)
+            
+            %Generate radom data with 0 meand Identity covariance N(0,I)
+            
+            XN = randn(nobs, nvar);
+            
+            %Compute covariance/correlation matrix
+            switch lower(type)
+                case 'covariance'
+                    CN = XN'*XN;
+                case 'correlation'
+                    CN = corr(XN, 'type', corrtype);
+                otherwise
+                    error('Type not supported')
+            end
+            
+            %Calculate eigenvalues
+            RAND_EIG(qq,:) = sort(eig(CN),'descend')';
+            
+        end
+        
+    otherwise
+         error('Method not supported')
+        
+end
+
+clc
+disp('Calculated percentiles')
+
+%% Calculate percentiles for PA
+ALPHA = (1-alpha)*100;
+PERCENTILES = prctile(RAND_EIG,ALPHA);
+
+%% Compute covariance/correlation of the input data matrix X
+switch lower(type)
+    case 'covariance'
+        C = X'*X;
+    case 'correlation'
+        C = corr(X, 'type', corrtype);
+end
+
+%% Compute sample eigenvalue (eigenvalues of input data matrix X)
+Eigs2test = sort(eig(C),'descend')';
+
+%% Print sample eigenvalues and corresponding PA percentiles
+for q = 1 : length(Eigs2test)
+    
+    disp(sprintf('%i. Sample eigenvalue %4.3f - Percentile %4.3f',q,Eigs2test(q),PERCENTILES(q)))
+    
+end
+
+%% Calculate difference between sample eigenvalues and percentiles and 
+%  extract the number of significant components
+
+DeltaEigs = Eigs2test-PERCENTILES;
+
+DeltaEigs(find(DeltaEigs>0)) = 1; 
+DeltaEigs(find(DeltaEigs<=0)) = 0;
+
+DeltaEigs(find(DeltaEigs<1,1):end) = 0;
+
+NoComp = sum(DeltaEigs);
+
+disp(sprintf('# of components: %i at alpha %2.2f', NoComp,alpha))
+
+%%
+if dispopt > 0 
+    %nvar2plot = min(NoComp+10,nvar);
+    nvar2plot = nvar;
+figure(1)
+hold on
+grid on; box on
+plot(1:nvar2plot,Eigs2test(1:nvar2plot),'.-','MarkerSize',22)
+plot(1:nvar2plot,PERCENTILES(1:nvar2plot),'r.-','MarkerSize',22)
+xlim([1-0.5 nvar+0.5])
+ax = gca;
+ax.XTick = [1:1:nvar2plot];
+ax.XTickLabel = 1:nvar2plot;
+xlabel('Order', 'FontSize',13)
+ylabel('Eigenvalue', 'FontSize',13)
+legend({sprintf('Original data (%s)',type),sprintf('1-%2.2f percentile %s',alpha, method)},'FontSize',14)
+
+end
---
+++ dray.m
@@ -0,0 +1,152 @@
+function [npcf,rvdim,rvdimperm]=dray(x,flagprep,npermmax,conf)
+
+% Dray's method for PCA component selection. If using this software please cite:
+% S. Dray, On the number of principal components: a test of dimensionality
+% based on measurements of similarity between matrices, Computational
+% Statistics & Data Analysis, 2008 (52), 2228-2237
+%
+% R. Vitale, J.A. Westerhuis, T. Naes, A.K. Smilde, O.E. de Noord, A.
+% Ferrer, Selecting the number of components in Principal Component
+% Analysis by permutation testing, Journal of Chemometrics, 2017 (31),
+% e2937
+%
+% npcf = dray(x) % minimum call
+% [npcf,rvdim,rvdimperm]=dray(x,flagprep,npermmax,conf)
+% % complete call
+%
+%
+% INPUTS:
+%
+% x: [NxM] original data
+%
+% flagprep: [1x1] 0 for no preprocessing on x; 1 for mean-centering; 2 for
+% auto-scaling. Default: 2
+%
+% npermmax: [1x1] maximum number of permutations. Default: 300
+%
+% conf: [1x1] confidence level for the test (e.g. 95 for 95%). Default: 95
+%
+%
+% OUTPUTS:
+%
+% npcf: [1x1] estimated number of PCA factors
+%
+% rvdim: [1xrank(x)] RVDIM statistic values for all the extractable
+% components of x
+%
+% rvdimperm: [(npermmax)x(npcf+1)] empirical null-distributions of the
+% RVDIM statistic for all the significant components plus one.
+%
+%
+% EXAMPLE OF USE: Random data
+%
+% x = simuleMV(20,10,8);
+% npcf = dray(x,2,300,95);
+%
+%
+% codified by: Raffaele Vitale (rvitale86@gmail.com)
+% last modification: 29/Aug/2018
+%
+% Copyright (C) 2016  Universitat Politecnica de Valencia, Valencia
+% Copyright (C) 2016  Raffaele Vitale
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%% Parameter checking and initialisation
+
+if nargin<1
+    error('Error in the number of arguments. Type ''help dray'' for more info.')
+end
+if nargin<2 || isempty(flagprep)
+    flagprep=2;
+end
+if nargin<3 || isempty(npermmax)
+    npermmax=300;
+end
+if nargin<4 || isempty(conf)
+    conf=95;
+end
+
+%% Main code
+
+% Preprocessing
+
+if flagprep==0
+    x_p=x;
+elseif flagprep==1
+    x_p=x-repmat(mean(x),size(x,1),1);
+elseif flagprep==2
+    x_p=(x-repmat(mean(x),size(x,1),1))./repmat(std(x),size(x,1),1);
+end
+
+% Singular Value Decomposition of x
+
+[u,s,v]=svd(x_p);
+t=u*s;
+eigreal=diag(s.^2)';
+
+rkeff=rank(x_p);
+for npc=1:rkeff
+    rvdim(npc)=(eigreal(npc))/sqrt(sum(eigreal(npc:rkeff).^2));
+end
+
+% Test for the first component
+
+for nperm=1:npermmax
+    
+    for nvar=1:size(x_p,2)
+        xperm(:,nvar)=x_p(randperm(size(x_p,1)),nvar);
+    end
+    
+    [~,sperm,~]=svd(xperm);
+    eigperm=diag(sperm.^2);
+    rvdimperm(nperm,1)=(eigperm(1))/sqrt(sum(eigperm(1:rkeff).^2));
+    
+end
+
+% Test for the a-th component (a>1)
+
+while size(rvdimperm,2)<size(rvdim,2) && rvdim(size(rvdimperm,2))>prctile(rvdimperm(:,end),100-(conf/size(rvdimperm,2)))
+
+    e=x_p-t(:,1:size(rvdimperm,2))*v(:,1:size(rvdimperm,2))';
+    
+    for nperm=1:npermmax
+    
+        for nvar=1:size(e,2)
+            eperm(:,nvar)=e(randperm(size(e,1)),nvar);
+        end
+        
+        [~,sresperm,~]=svd(eperm);
+        eigresperm=diag(sresperm.^2);
+        rvdimperm_tmp(nperm,1)=eigresperm(1)/sqrt(sum(eigresperm.^2));
+        
+    end
+    
+    rvdimperm=[rvdimperm rvdimperm_tmp];
+    
+end
+
+if size(rvdimperm,2)==size(rvdim,2) && rvdim(size(rvdimperm,2))>prctile(rvdimperm(:,end),100-(conf/size(rvdimperm,2)))
+    
+    npcf=size(rvdim,2);
+    
+else
+
+    npcf=size(rvdimperm,2)-1;
+    
+end
+
+%% Output display
+
+disp(['The number of significant components is ',num2str(npcf)]);
