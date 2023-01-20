8669d5766fa308abe0b94c3518ed2de88c4516fa
Iecolab UGR <mdarmstr@ualberta.ca>
2023-01-20 16:02:25 +0100

    trying a new method of handling labels that are long

--- plot_vec.m
+++ plot_vec.m
@@ -8,11 +8,11 @@ function fig_h = plot_vec(vec,elabel,classes,xylabel,lcont,opt,vlabel,mult,maxv)
 %
 % INPUTS:
 %
-% vec: [NxM] vector/s to plot. 
+% vec: [NxM] vector/s to plot.
 %
 % elabel: [Nx1] name of the vector elements (numbers are used by default)
 %
-% classes: [Nx1, str(N), {N}] groups for different visualization (a single 
+% classes: [Nx1, str(N), {N}] groups for different visualization (a single
 %   group by default)
 %
 % xylabel: {2} xlabel and ylabel (nothing by default)
@@ -56,20 +56,20 @@ function fig_h = plot_vec(vec,elabel,classes,xylabel,lcont,opt,vlabel,mult,maxv)
 %
 % Copyright (C) 2022  University of Granada, Granada
 % Copyright (C) 2022  Jose Camacho Paez
-% 
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
+
 %% Parameters checking
 
 % Set default values
@@ -118,17 +118,17 @@ assert (isequal(size(opt), [1 1]), 'Dimension Error: 6th argument must be 1-by-1
 if ~isempty(vlabel), assert (isequal(size(vlabel), [M 1]), 'Dimension Error: 7th argument must be M-by-1. Type ''help %s'' for more info.', routine(1).name); end;
 if ~isempty(mult), assert (isequal(size(mult), [N 1]), 'Dimension Error: 8th argument must be N-by-1. Type ''help %s'' for more info.', routine(1).name); end;
 if ~isempty(maxv), assert (isequal(size(maxv), [1 3]), 'Dimension Error: 9th argument must be 1-by-3. Type ''help %s'' for more info.', routine(1).name); end;
- 
+
 % Validate values of input data
 assert (isempty(find(opt~='0' & opt~='1')), 'Value Error: 6th argument must contain a binary value. Type ''help %s'' for more info.', routine(1).name);
-   
+
 % Convert constant limits in vectors
 if ~isempty(lcont) && ~isequal(size(lcont,1), N), lcont = (lcont*ones(1,N))'; end;
-    
-% Exception: bar plot with multivariate vec and one-observation class  
+
+% Exception: bar plot with multivariate vec and one-observation class
 if ~opt && ~isempty(classes) && size(vec, 2)>1,
     unique_classes = unique(classes);
-    assert (min(hist(classes,unique(classes)))>1, 'Exception: Cannot visualize a multivariate bar plot with one-observation classes. Try setting the 6th argument to 1.'); 
+    assert (min(hist(classes,unique(classes)))>1, 'Exception: Cannot visualize a multivariate bar plot with one-observation classes. Try setting the 6th argument to 1.');
 end;
 
 %% Main code
@@ -173,7 +173,7 @@ if ~isempty(classes)
         else
             plot(vec,'k','HandleVisibility', 'off');
         end
-    end  
+    end
     for i=1:length(unique_classes)
         ind = ord_classes == unique_ord_classes(i);
         if isnumeric(elabel) && length(elabel)==length(unique(elabel))
@@ -181,14 +181,14 @@ if ~isempty(classes)
         else
             vind = find(ind);
         end
-            
-        
+
+
         if opt == '0',
             plot(vind, vec(ind,:), 'Color', 'none', 'Marker','O', 'MarkerFaceColor', color_list(i,:), 'DisplayName', unique_classes{i});
-        else 
+        else
            bar([0;vind;max(vind)+1], [zeros(1,size(vec,2));vec(ind,:);zeros(1,size(vec,2))], 0.8, 'FaceColor', color_list(i,:), 'EdgeColor', 'none', 'DisplayName', unique_classes{i});
         end
-    end 
+    end
 else
     color_list = hsv(M);
     for i=1:M,
@@ -205,7 +205,7 @@ else
                 bar(vec(:,i), 'FaceColor', color_list(i,:), 'EdgeColor', 'none', 'DisplayName', vlabel{i});
             end
         end
-    end    
+    end
 end
 
 % Plot control limits
@@ -216,7 +216,7 @@ if ~isempty(lcont)
         a = [lcont(:,i)';lcont(:,i)'];
         plot(b(2:(end-1))',a(:),'r--','LineWidth',2,'HandleVisibility', 'off');
     end
-end    
+end
 
 % Get axes handler
 axes_h = get(fig_h,'Children');
@@ -226,34 +226,45 @@ for i=1:length(axes_h)
         val=i;
     end
 end
-axes_h = axes_h(i); 
+axes_h = axes_h(i);
 
 % Set ticks and labels
-if ~isempty(elabel) 
-    lablength = cellfun('length', elabel);
-    label_length = max(lablength(1:end-1)+lablength(2:end))/2;
-    label_sizeH = 5/label_length;
-    label_sizeV = 5;
-    MaxRot = 60;
-    set(axes_h, 'FontSize', max(min(14,round(label_sizeH)), 10));
-    if 0.2*N<label_sizeH % labels do not need to be rotated
-        vals = 1:N;
-        set(axes_h,'XTick',vals);
-        set(axes_h,'XTickLabel',elabel(vals));
-    elseif 0.2*N<label_sizeV % labels are rotated
-        vals = 1:N;
-        set(axes_h,'XTick',vals);
-        set(axes_h,'XTickLabel',elabel(vals));
-        set(axes_h,'XTickLabelRotation',ceil(MaxRot*0.2*N/label_sizeV));
-    else % labels are reduced
-        set(axes_h,'XTickMode','auto');
-        ind = get(axes_h,'XTick');
-        ind2 = find(ind>0&ind<=length(elabel));
-        set(axes_h,'XTick',ind(ind2));
-        set(axes_h,'XTickLabel',elabel(ind(ind2)));
-        set(axes_h,'XTickLabelRotation',MaxRot);
-        set(axes_h, 'FontSize', 14);
-    end
+%if ~isempty(elabel)
+%    lablength = cellfun('length', elabel);
+%    label_length = max(lablength(1:end-1)+lablength(2:end))/2;
+%    label_sizeH = 5/label_length;
+%    label_sizeV = 5;
+%    MaxRot = 60;
+%    set(axes_h, 'FontSize', max(min(14,round(label_sizeH)), 10));
+%    if 0.2*N<label_sizeH % labels do not need to be rotated
+%        vals = 1:N;
+%        set(axes_h,'XTick',vals);
+%        set(axes_h,'XTickLabel',elabel(vals));
+%    elseif 0.2*N<label_sizeV % labels are rotated
+%        vals = 1:N;
+%        set(axes_h,'XTick',vals);
+%        set(axes_h,'XTickLabel',elabel(vals));
+%        set(axes_h,'XTickLabelRotation',ceil(MaxRot*0.2*N/label_sizeV)); %XTickLabelRotation is not avaialble in Octave
+%    else % labels are reduced
+%        set(axes_h,'XTickMode','auto');
+%        ind = get(axes_h,'XTick');
+%        ind2 = find(ind>0&ind<=length(elabel));
+%        set(axes_h,'XTick',ind(ind2));
+%        set(axes_h,'XTickLabel',elabel(ind(ind2)));
+%        set(axes_h,'XTickLabelRotation',MaxRot);
+%        set(axes_h, 'FontSize', 14);
+%    end
+%end
+
+%MDSA 20230120 - trying to find a general solution to the text labels.
+if ~isempty(elabel)
+  lablength = cellfun('length', elabel);
+  label_length = max(lablength(1:end-1)+lablength(2:end))/2;
+  label_sizeH = label_length;
+  font_size = max(min(14,round(label_sizeH)), 10) * 0.8
+  for ii = 1:length(vec)
+    text(ii,mean([0,vec(ii)])*0.8,elabel{ii},'rotation',90,'horizontalalignment','left','FontSize',font_size);
+  end
 end
 
 if ~isempty(xylabel)
