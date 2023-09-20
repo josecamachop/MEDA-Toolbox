c36ec8611fbdaad46dbddb984f11f80d2360ec52
Michael D. Sorochan Armstrong <45431766+mdarmstr@users.noreply.github.com>
2023-09-20 14:22:44 +0200

    Update table2latex.m
    Just fixing an error with the table2latex code. Not tested with Octave, but there seem to be some ongoing compatibility issues.

--- table2latex.m
+++ table2latex.m
@@ -10,48 +10,49 @@ function table2latex(T,fileName)
 isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 
 if ~isOctave
-  T2.source = table2cell(T(:,'Source'));
-  T2.var = T.Properties.VariableNames;
-  T2.mat = table2array(T(:,2:end))
-  T = T2;
+    T2.source = table2cell(T(:,'Source'));
+    T2.var = T.Properties.VariableNames;
+    T2.mat = table2array(T(:,2:end));
+    T = T2;
 end
 
-  fid = fopen (fileName, "w");
-  fprintf(fid,"\\begin{table} \n");
-  numCol = size(T.var,2) + 1;
-  fprintf(fid,"\\begin{tabular}{");
-  fprintf(fid,repmat('l',[1 numCol]));
-  fprintf(fid,"}\n");
-  for ii = 1:size(T.mat,1)+1
+fid = fopen(fileName, "w");
+fprintf(fid,"\\begin{table} \n");
+numCol = size(T.var,2) + 1;
+fprintf(fid,"\\begin{tabular}{");
+fprintf(fid,repmat('l',[1 numCol]));
+fprintf(fid,"}\n");
+for ii = 1:size(T.mat,1)+1
     for jj = 1:size(T.mat,2)+1
-      if jj == 1 && ii == 1
-        fprintf(fid, " &");
-        fprintf(fid, " ");
-      elseif jj == 1
-        fprintf(fid,strcat(T.source{ii-1,1}," &"));
-        fprintf(fid," ");
-      elseif ii == 1 && jj == size(T.mat,2)+1
-        fprintf(fid, T.var{1,jj-1});
-        fprintf(fid," ");
-      elseif ii == 1
-        fprintf(fid, strcat(T.var{1,jj-1}," &"));
-        fprintf(fid," ");
-      elseif jj == size(T.mat,2)+1
-        fprintf(fid, num2str(T.mat(ii-1,jj-1)));
-        fprintf(fid," ");
-      else
-        fprintf(fid,strcat(num2str(T.mat(ii-1,jj-1))," &"));
-        fprintf(fid," ");
-      end
+        if jj == 1 && ii == 1
+            fprintf(fid, " &");
+            fprintf(fid, " ");
+        elseif jj == 1
+            fprintf(fid,strcat(T.source{ii-1,1}," &"));
+            fprintf(fid," ");
+        elseif ii == 1 && jj == size(T.mat,2)+1
+            fprintf(fid, T.var{1,jj});
+            fprintf(fid," ");
+        elseif ii == 1
+            fprintf(fid, strcat(T.var{1,jj}," &"));
+            fprintf(fid," ");
+        elseif jj == size(T.mat,2)+1
+            fprintf(fid, num2str(T.mat(ii-1,jj-1)));
+            fprintf(fid," ");
+        else
+            fprintf(fid,strcat(num2str(T.mat(ii-1,jj-1))," &"));
+            fprintf(fid," ");
+        end
     end
+    
     if ii == 1
-      fprintf(fid,"\\\\ \n");
-      fprintf(fid," \\hline \n");
+        fprintf(fid,"\\\\ \n");
+        fprintf(fid," \\hline \n");
     else
-      fprintf(fid,"\\\\ \n");
-    endif
-  end
-  fprintf(fid,"\\end{tabular} \n");
-  fprintf(fid,"\\end{table} \n");
-  fclose(fid);
+        fprintf(fid,"\\\\ \n");
+    end
 end
+
+fprintf(fid,"\\end{tabular} \n");
+fprintf(fid,"\\end{table} \n");
+fclose(fid);
