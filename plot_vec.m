b91f67878ac87aacf9685a0c8d6bb1b2c7a219d5
José Camacho <josecamacho@ugr.es>
2024-04-12 08:53:39 +0200

    Update plot_vec.m

--- plot_vec.m
+++ plot_vec.m
@@ -3,14 +3,14 @@ function fig_h = plot_vec(vec,varargin)
 % Bar or line plot.
 %
 % plot_vec(vec) % minimum call
-% plot_vec(vec,'EleLabel',elabel,'ObsClass',classes,'XYLabel',xylabel,'LimCont',lcont,'Option',opt,'VecLabel',vlabel,'Multiplicity',mult,'Threshold',maxv) % complete call
+% plot_vec(vec,'PARAM1',val1,'PARAM2',val2,...)
 %
 %
 % INPUTS:
 %
 % vec: [NxM] vector/s to plot. 
 %
-% Optional INPUTS:
+% Optional INPUTS (Parameters):
 %
 % 'EleLabel': [Nx1] name of the vector elements (numbers are used by default)
 %
@@ -29,14 +29,14 @@ function fig_h = plot_vec(vec,varargin)
 %           0: plot for numerical classes (consistent with a colorbar)
 %           1: plot for categorical classes (consistent with a legend)
 %
-%   By deafult, opt = '10'. If less digits are specified, least significant
+%   By deafult, opt = '11'. If less digits are specified, least significant
 %   digits are set to 0, i.e. opt = 1 means a=1, b=0
 %
 % 'VecLabel': [Mx1] name of the vectors (numbers are used by default)
 %
 % 'Multiplicity': [NxM] multiplicity of each row (1s by default)
 %
-% 'Threshold': [1x3] thresholds for the different marker size (20, 50 and 100 by default)
+% 'Markers': [1x3] thresholds for the different marker size (20, 50 and 100 by default)
 %
 %
 % OUTPUTS:
@@ -51,17 +51,17 @@ function fig_h = plot_vec(vec,varargin)
 %
 % EXAMPLE OF USE: with labels and classes in observations and variable limit:
 %
-% fig_h = plot_vec(randn(5,3),'EleLabel',{'one','two','three','four','five'},'ObsClass',[1 1 1 2 2],'XYLabel',{[],'Functions'},'LimCont',randn(5,1),'Option',1);
+% fig_h = plot_vec(randn(5,3),'EleLabel',{'one','two','three','four','five'},'ObsClass',[1 1 1 2 2],'XYLabel',{[],'Functions'},'LimCont',randn(5,1),'Option','11');
 %
 %
 % EXAMPLE OF USE: with labels, multiplicity and classes in observations and variable limit:
 %
-% fig_h = plot_vec(randn(5,3),'EleLabel',{'one','two','three','four','five'},'ObsClass',[1 1 1 2 2],'XYLabel',{[],'Functions'},'LimCont',randn(5,1),'Option',1,'VecLabel',100*rand(5,1),'Multiplicity',[20 50 100]);
+% fig_h = plot_vec(randn(5,3),'EleLabel',{'one','two','three','four','five'},'ObsClass',[1 1 1 2 2],'XYLabel',{[],'Functions'},'LimCont',randn(5,1),'Option',11,'Multiplicity',100*rand(5,1),'Markers',[20 50 100]);
 %
 %
 % coded by: Jose Camacho Paez (josecamacho@ugr.es)
 %           Alejandro Perez Villegas (alextoni@gmail.com)
-% last modification: 11/Apr/2024
+% last modification: 12/Apr/2024
 %
 % Copyright (C) 2024  University of Granada, Granada
 % 
@@ -86,14 +86,6 @@ assert (nargin >= 1, 'Error in the number of arguments. Type ''help %s'' for mor
 if size(vec,1) == 1,     vec = vec'; end;
 N = size(vec, 1);
 M = size(vec, 2);
-% if nargin < 2 || isempty(elabel), elabel = 1:N; end;
-% if nargin < 3 || isempty(classes), classes = []; end;
-% if nargin < 4 || isempty(xylabel), xylabel = {'',''}; end;
-% if nargin < 5 || isempty(lcont),  lcont = []; end;
-% if nargin < 6 || isempty(opt),  opt = '1'; end;
-% if nargin < 7 || isempty(vlabel),  vlabel = 1:M; end;
-% if nargin < 8 || isempty(mult),    mult    = ones(N,1);         end;
-% if nargin < 9 || isempty(maxv),    maxv    = [20 50 100];       end;
 
 % Introduce optional inputs as parameters (name-value pair) 
 p = inputParser;
@@ -101,9 +93,9 @@ addParameter(p,'EleLabel',1:N);
 addParameter(p,'ObsClass',[]);
 addParameter(p,'XYLabel',{'',''});
 addParameter(p,'LimCont',[]);
-addParameter(p,'Option','1');
+addParameter(p,'Option','11');
 addParameter(p,'Multiplicity',ones(N,1));
-addParameter(p,'Threshold',[20 50 100]);
+addParameter(p,'Markers',[20 50 100]);
 addParameter(p,'VecLabel',1:M);
 parse(p,varargin{:});
 
@@ -114,7 +106,7 @@ xylabel = p.Results.XYLabel;
 lcont = p.Results.LimCont;
 opt = p.Results.Option;
 mult = p.Results.Multiplicity;
-maxv = p.Results.Threshold;
+maxv = p.Results.Markers;
 vlabel = p.Results.VecLabel;
 
 % Convert num arrays to str
@@ -122,7 +114,7 @@ if isnumeric(opt), opt=num2str(opt); end
 
 % Correct for opt integrity
 while length(opt)<2, opt = strcat(opt,'0'); end
-if opt(2) == 0 & ~isnumeric(classes), opt(2) = 1; end
+if opt(2) == 0 && ~isnumeric(classes), opt(2) = 1; end
 
 % Convert row arrays to column arrays
 if size(elabel,1)  == 1, elabel = elabel'; end;
@@ -134,7 +126,7 @@ if size(maxv,2) == 1, maxv = maxv'; end;
 
 % Convert num arrays to str
 if ~isempty(vlabel) && isnumeric(vlabel), vlabel=num2str(vlabel); end
-if ~isempty(classes) && isnumeric(classes) && opt(1)=='1', classes=num2str(classes); end
+if ~isempty(classes) && isnumeric(classes) && opt(2)=='1', classes=num2str(classes); end
 
 % Convert char arrays to cell
 if ischar(elabel),  elabel = cellstr(elabel); end;
@@ -143,17 +135,18 @@ if ischar(xylabel),  xylabel = cellstr(xylabel); end;
 if ischar(vlabel),  vlabel = cellstr(vlabel); end;
 
 % Validate dimensions of input data
-if ~isempty(elabel), assert (isequal(size(elabel), [N 1]), 'Dimension Error: 2nd argument must be N-by-1. Type ''help %s'' for more info.', routine(1).name); end;
-if ~isempty(classes), assert (isequal(size(classes), [N 1]), 'Dimension Error: 3rd argument must be N-by-1. Type ''help %s'' for more info.', routine(1).name); end;
-if ~isempty(xylabel), assert (length(xylabel) == 2, 'Dimension Error: 4th argument must contain 2 cell elements. Type ''help %s'' for more info.', routine(1).name); end;
-if ~isempty(lcont), assert (isequal(size(lcont,1), N) || isequal(size(lcont,2), 1), 'Dimension Error: 5th argument must be N-by-L or L-by-1. Type ''help %s'' for more info.', routine(1).name); end;
-assert (ischar(opt) && length(opt)==2, 'Dimension Error: 6th argument must be a string or num of maximum 2 bits. Type ''help %s'' for more info.', routine(1).name);
-if ~isempty(vlabel), assert (isequal(size(vlabel), [M 1]), 'Dimension Error: 7th argument must be M-by-1. Type ''help %s'' for more info.', routine(1).name); end;
-if ~isempty(mult), assert (isequal(size(mult), [N 1]), 'Dimension Error: 8th argument must be N-by-1. Type ''help %s'' for more info.', routine(1).name); end;
-if ~isempty(maxv), assert (isequal(size(maxv), [1 3]), 'Dimension Error: 9th argument must be 1-by-3. Type ''help %s'' for more info.', routine(1).name); end;
+
+if ~isempty(elabel), assert (isequal(size(elabel), [N 1]), 'Dimension Error: parameter ''EleLabel'' must be N-by-1. Type ''help %s'' for more info.', routine(1).name); end;
+if ~isempty(classes), assert (isequal(size(classes), [N 1]), 'Dimension Error: parameter ''ObsClass'' must be N-by-1. Type ''help %s'' for more info.', routine(1).name); end;
+if ~isempty(xylabel), assert (length(xylabel) == 2, 'Dimension Error: parameter ''XYLabel'' must contain 2 cell elements. Type ''help %s'' for more info.', routine(1).name); end;
+if ~isempty(lcont), assert (isequal(size(lcont,1), N) || isequal(size(lcont,2), 1), 'Dimension Error: parameter ''LimCont'' must be N-by-L or L-by-1. Type ''help %s'' for more info.', routine(1).name); end;
+assert (ischar(opt) && length(opt)==2, 'Dimension Error: parameter ''Option'' must be a string or num of maximum 2 bits. Type ''help %s'' for more info.', routine(1).name);
+if ~isempty(vlabel), assert (isequal(size(vlabel), [M 1]), 'Dimension Error: parameter ''VecLabel'' must be M-by-1. Type ''help %s'' for more info.', routine(1).name); end;
+if ~isempty(mult), assert (isequal(size(mult), [N 1]), 'Dimension Error: parameter ''Multiplicity'' must be N-by-1. Type ''help %s'' for more info.', routine(1).name); end;
+if ~isempty(maxv), assert (isequal(size(maxv), [1 3]), 'Dimension Error: parameter ''Markers'' must be 1-by-3. Type ''help %s'' for more info.', routine(1).name); end;
  
 % Validate values of input data
-assert (isempty(find(opt~='0' & opt~='1')), 'Value Error: 6th argument must contain binary values. Type ''help %s'' for more info.', routine(1).name);
+assert (isempty(find(opt~='0' & opt~='1')), 'Value Error: parameter ''Option'' must contain binary values. Type ''help %s'' for more info.', routine(1).name);
    
 % Convert constant limits in vectors
 if ~isempty(lcont) && ~isequal(size(lcont,1), N), lcont = (lcont*ones(1,N))'; end;
@@ -170,7 +163,18 @@ end;
 fig_h = figure;
 hold on;
 
-% Preprocess classes to force them start with 1, 2...n,
+% Sort data for colorbar
+if opt(2)=='0'
+    [classes,ord] = sort(classes,'ascend');
+    cax = [min(classes) max(classes)];
+    classes = num2str(classes); 
+    classes = cellstr(classes);
+    vec = vec(ord,:);
+    elabel = elabel(ord);
+    mult = mult(ord);
+end
+
+% Get ordering of classes
 unique_classes = unique(classes,'stable');
 if iscell(classes)
     ord_classes = arrayfun(@(x) find(strcmp(unique_classes, x), 1), classes);
@@ -199,14 +203,11 @@ for j=1:length(bins)-1
 end
 
 if ~isempty(classes)
-    color_list = hsv(length(unique_ord_classes));
-    if opt == '0'
-        if isnumeric(elabel) && length(elabel)==length(unique(elabel))
-            plot(elabel,vec,'k','HandleVisibility', 'off');
-        else
-            plot(vec,'k','HandleVisibility', 'off');
-        end
-    end  
+    if opt(2) == '0'
+        color_list = parula(length(unique_ord_classes));
+    else
+        color_list = hsv(length(unique_ord_classes));
+    end
     for i=1:length(unique_ord_classes)
         ind = ord_classes == unique_ord_classes(i);
         if isnumeric(elabel) && length(elabel)==length(unique(elabel))
@@ -215,17 +216,16 @@ if ~isempty(classes)
             vind = find(ind);
         end
             
-        
-        if opt == '0'
-            plot(vind, vec(ind,:), 'Color', 'none', 'Marker','O', 'MarkerFaceColor', color_list(i,:), 'DisplayName', unique_classes{i});
+        if opt(1) == '0'
+           plot(vind, vec(ind,:), 'Color', 'none', 'Marker','O', 'MarkerFaceColor', color_list(i,:), 'DisplayName', unique_classes{i});
         else 
-           bar([0;vind;max(vind)+1], [0;vec(ind,:);0], 0.8, 'FaceColor', color_list(i,:), 'EdgeColor', 'none', 'DisplayName', unique_classes{i});
+           bar(vind, vec(ind,:), 0.8, 'FaceColor', color_list(i,:), 'EdgeColor', 'none', 'DisplayName', unique_classes{i});
         end
     end 
 else
     color_list = hsv(M);
     for i=1:M
-        if opt == '0'
+        if opt(1) == '0'
             if isnumeric(elabel) && length(elabel)==length(unique(elabel))
                 plot(elabel, vec(:,i), 'LineWidth', 2, 'Color', color_list(i,:), 'DisplayName', vlabel{i});
             else
@@ -245,7 +245,7 @@ end
 if ~isempty(lcont)
     hold on
     b = [0.5:(N+1);0.5:(N+1)];
-    for i=1:size(lcont,2),
+    for i=1:size(lcont,2)
         a = [lcont(:,i)';lcont(:,i)'];
         plot(b(2:(end-1))',a(:),'r--','LineWidth',2,'HandleVisibility', 'off');
     end
