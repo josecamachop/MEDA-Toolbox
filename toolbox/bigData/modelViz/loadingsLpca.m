66e7572dfef4934fedd3afb11a8cad3e970f5128
JesusGarSan <jesusgarsan7@gmail.com>
2025-04-25 12:42:25 +0200

    fixed typo error

--- toolbox/bigData/modelViz/loadingsLpca.m
+++ toolbox/bigData/modelViz/loadingsLpca.m
@@ -105,7 +105,7 @@ P = Lmodel.loads;
 figH = [];
 if opt(1) == '1'
     
-    tvar = varLpca(Lmodel,'Plot',falsegit );
+    tvar = varLpca(Lmodel,'Plot',false);
     
     if length(Lmodel.lvs) == 1 || opt(2) == '1'
         for i=1:length(Lmodel.lvs)
