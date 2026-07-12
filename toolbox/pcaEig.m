2aefc23717d383ce38a08af3d3517603e1e4e433
José Camacho <josecamacho@ugr.es>
2026-07-12 21:34:55 +0200

    Update pcaEig.m

--- toolbox/pcaEig.m
+++ toolbox/pcaEig.m
@@ -31,7 +31,7 @@ function model = pcaEig(xcs,varargin)
 % model = pcaEig(Xcs,'PCs',pcs)
 %
 %
-% EXAMPLE OF USE: Compare PCA algorithms 
+% EXAMPLE OF USE: Compare PCA algorithms for wide data
 %
 % X = randn(20,1e4);
 % Xcs = preprocess2D(X,'Preprocessing',2);
@@ -39,14 +39,23 @@ function model = pcaEig(xcs,varargin)
 % tic, coefSvd = pca(Xcs,'Algorithm','svd','Centered',false,'NumComponents',2); toc
 % tic, coefEig = pca(Xcs,'Algorithm','eig','Centered',false,'NumComponents',2); toc
 %
+% for pc = 1:2,
+%   s = sign(model.loads(abs(model.loads(:,pc))==max(abs(model.loads(:,pc))),pc));
+%   model.loads(:,pc) = s*model.loads(:,pc);
+%   s = sign(coefSvd(abs(coefSvd(:,pc))==max(abs(coefSvd(:,pc))),pc));
+%   coefSvd(:,pc) = s*coefSvd(:,pc);
+%   s = sign(coefEig(abs(coefEig(:,pc))==max(abs(coefEig(:,pc))),pc));
+%   coefEig(:,pc) = s*coefEig(:,pc);
+% end
+%
 % norm(model.loads-coefSvd)
 % norm(coefEig-coefSvd)
 % norm(model.loads-coefEig)
 %
 %
 % coded by: Jose Camacho (josecamacho@ugr.es)
-% last modification: 9/Jun/2026
-% Dependencies: Matlab R2024b, MEDA v1.13
+% last modification: 12/Jul/2026
+% Dependencies: Matlab R2024b, MEDA v1.14
 %
 % Copyright (C) 2026  University of Granada, Granada
 % 
@@ -73,7 +82,7 @@ M = size(xcs, 2);
 
 % Introduce optional inputs as parameters (name-value pair) 
 p = inputParser;
-addParameter(p,'PCs',0:rank(xcs));   
+addParameter(p,'PCs',1:min(N,M));   
 parse(p,varargin{:});
 
 % Extract inputs from inputParser for code legibility
@@ -84,16 +93,14 @@ if size(pcs,2) == 1, pcs = pcs'; end
 
 % Preprocessing
 pcs = unique(pcs);
-pcs(find(pcs==0)) = [];
-pcs(find(pcs>size(xcs,2))) = [];
-pcs(find(pcs>rank(xcs))) = [];
+pcs(find(pcs>min(N,M))) = [];
 A = length(pcs);
 
 % Validate dimensions of input data
 assert (isequal(size(pcs), [1 A]), 'Dimension Error: parameter ''Pcs'' must be 1-by-A. Type ''help %s'' for more info.', routine(1).name);
 
 % Validate values of input data
-assert (isempty(find(pcs<0)) && isequal(fix(pcs), pcs), 'Value Error: parameter ''Pcs'' must contain positive integers. Type ''help %s'' for more info.', routine(1).name);
+assert (isempty(find(pcs<=0)) && isequal(fix(pcs), pcs), 'Value Error: parameter ''Pcs'' must contain positive integers. Type ''help %s'' for more info.', routine(1).name);
 
 
 %% Main code
@@ -101,15 +108,14 @@ assert (isempty(find(pcs<0)) && isequal(fix(pcs), pcs), 'Value Error: parameter 
 if N>M
     XX = xcs'*xcs;
     [p,D] = eig(1/(N-1)*XX);
-    [lambda,ind] = sort(real(diag(D)),'descend');
+    [lambda,ind] = sort(diag(D),'descend');
     p = p(:,ind);
     t = xcs*p;
 else
     XX = xcs*xcs';
     [t,D] = eig(1/(N-1)*XX);
-    s = real(sqrt(real(diag(D))));
-    [lambda,ind] = sort(s,'descend');
-    t = t(:,ind).*(sqrt((N-1))*ones(N,1)*s(ind)');
+    [lambda,ind] = sort(diag(D),'descend');
+    t = t(:,ind).*(sqrt((N-1))*ones(N,1)*real(sqrt(lambda')));
     p = xcs'*t;
     for i=1:size(p,2)
         p(:,i) = p(:,i)/sqrt(p(:,i)'*p(:,i));
